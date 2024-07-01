107dba5ab1e7f8baa6a64277cd6cb0d7e7e97734
David Fernandez Gonzalez (litios) <david.fernandezgonzalez@canonical.com>
2024-07-01 09:23:52 +0200

    Init
    Signed-off-by: David Fernandez Gonzalez (litios) <david.fernandezgonzalez@canonical.com>

---
+++ config-wm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# https://github.com/addy-dclxvi/i3-starterpack
+# 
+sudo apt update
+sudo apt install -y i3 polybar rofi pulseaudio i3-wm dunst i3lock i3status suckless-tools
+sudo apt install -y fonts-noto fonts-mplus xsettingsd lxappearance scrot viewnior hsetroot
